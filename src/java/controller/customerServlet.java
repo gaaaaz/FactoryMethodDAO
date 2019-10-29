b4576837989556e35293fe017a310c1429513f0d
AULAFISI <AULAFISI@F20LAB304-5E.UNMSM.pe>
2019-10-29 17:55:21 -0500

    Lab_06 JSP

---
+++ src/java/controller/customerServlet.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import dao.DAOFactory;
+import dao.design.ICountryDAO;
+import dao.design.ICustomerDAO;
+import dao.ds.DBAccess;
+import dao.objects.Country;
+import dao.objects.Customer;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author AULAFISI
+ */
+@WebServlet(name = "customerServlet", urlPatterns = {"/customer_per_country"})
+public class customerServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet customerServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet customerServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        ICountryDAO countryDAO = DAOFactory.getInstance().getCountryDAO();
+        List<Country> c = countryDAO.getCountries();
+        
+        request.setAttribute("countries", c);
+        request.getRequestDispatcher("startPage.jsp");
+        
+        ICustomerDAO customerDAO = DAOFactory.getInstance().getCustomerDAO();
+        List<Customer> cus = customerDAO.getCustomerPerCountry(10);
+        request.setAttribute("customers", cus);
+        request.getRequestDispatcher("startPage.jsp").forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        ICustomerDAO customerDAO = DAOFactory.getInstance().getCustomerDAO();
+        customerDAO.getCustomerPerCountry(2);
+        
+        
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- web/startPage.jsp
+++ web/startPage.jsp
@@ -1,17 +1,74 @@
-<%-- 
-    Document   : startPage
-    Created on : 29-oct-2019, 15:08:39
-    Author     : AULAFISI
---%>
 
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <title>Laboratorio 06 - Factory Method</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
+
+        <nav class="navbar navbar-dark bg-dark">
+            <a class="navbar-brand" href="#">Sakila</a>
+        </nav>
+
+        <div class="container">
+            <br>
+            <div class="row">
+                <h1>Lista de clientes por país</h1>
+            </div>
+
+            <br>
+
+            <form method="POST" action="customerServlet">
+
+                <div class="row">
+                    <div class="form-group col-8">
+                        <label for="exampleFormControlSelect1">Example select</label>
+                        <select class="form-control" id="exampleFormControlSelect1">
+                            <c:forEach items="${countries}" var="item">
+                                <option value="${item.getCountry_id()}">${item.getCountry()}</option>
+                            </c:forEach>
+                        </select>
+                    </div>
+
+                    <div class="col-4 mt-4">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                </div>
+
+
+
+            </form>
+
+            <br>
+
+            <table class="table">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Apellido</th>
+                        <th scope="col">Ciudad</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <c:forEach items="${customers}" var="item">
+                        <tr>
+                            <th scope="row">${item.getCustomer_id()}</th>
+                            <td>${item.getFirst_name()}</td>
+                            <td>${item.getLast_name()}</td>
+                            <td>${item.getAddress().getCity().getCity()}</td>
+                        </tr>
+                    </c:forEach>
+
+
+                </tbody>
+            </table>
+
+        </div>
+
     </body>
 </html>
